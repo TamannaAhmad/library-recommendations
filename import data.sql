7d2575d00741f575ec29b476df8b892a0d763f7f
Tamanna <tamanna.ahmad71711@gmail.com>
2024-12-15 16:53:06 +0530

    added datasheets and sql import files

---
+++ import data.sql
@@ -0,0 +1,16 @@
+create database library_system;
+use library_system;
+create table books (book_id int unique, title varchar(100), author varchar(100), edition int, pub_year year);
+load data local infile 'C:/Users/gryff/OneDrive/Documents/Tamanna/mini-project/data/books.csv' into table books fields terminated by ',' enclosed by '"' ignore 1 rows;
+select * from books;
+drop table courses;
+create table courses (course_code varchar(20), course_name varchar(100), primary key(course_code));
+load data local infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/courses.csv' into table courses fields terminated by ',' enclosed by '"' ignore 1 rows;
+select * from courses;
+create table branch_course (course_code varchar(20), branch varchar(10), semester int, foreign key (course_code) references courses(course_code));
+load data local infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/branch-course.csv' into table branch_course fields terminated by ',' enclosed by '"' ignore 1 rows;
+select * from branch_course;
+create table courses_books (course_code varchar(20), book_id int);
+load data local infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/courses-books.csv' into table courses_books fields terminated by ',' enclosed by '"' ignore 1 rows;
+select * from courses_books;
+delete from courses_books where book_id = 0;
